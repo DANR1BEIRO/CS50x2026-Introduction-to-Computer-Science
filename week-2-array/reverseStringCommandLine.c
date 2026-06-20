d94d480c4745532b0cbd6c977c18b92d9f23afcc
DANR1BEIRO <danribeirodev@gmail.com>
2026-06-20 19:47:49 -0300

    Week 2 - Arrays - section 2: reverse a command-line input string

---
+++ week-2-array/reverseStringCommandLine.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+int main(int argc, string argv[])
+{
+    if (argc = 0)
+    {
+        return 1;
+    }
+
+    int n = strlen(argv[1]);
+    for (int i = n; i >= 0; i--)
+    {
+        printf("%c", argv[1][i]);
+    }
+    printf("\n");
+}
